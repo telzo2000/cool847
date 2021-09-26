29985d1bd63aa43a5b1f63eb2d14cc84905652ea
telzo2000 <67196597+telzo2000@users.noreply.github.com>
2021-09-26 19:32:13 +0900

    Add files via upload

--- firmware/cool847/keymaps/default/keymap.c
+++ firmware/cool847/keymaps/default/keymap.c
@@ -43,10 +43,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
     
     [_BASE] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,KC_BSPC,\
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_MINUS, KC_ENT, \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    LT(_UPPER,KC_V),LT(_LOWER,KC_B),LT(_LOWER,KC_B),LT(_FN,KC_N),KC_M,    KC_COMM, KC_DOT,  KC_UP,    KC_RSFT, \
-        KC_LGUI, KC_LALT,          KC_SPACE,LT(_FN,KC_SPACE),LT(_LOWER,KC_ENTER),LSFT_T(KC_SPACE),  KC_LEFT,KC_DOWN,KC_RIGHT \
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,\
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_MINUS, KC_BSPC, \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    LT(_UPPER,KC_V),LT(_LOWER,KC_B),LT(_LOWER,KC_B),LT(_FN,KC_N),KC_M,    KC_COMM, KC_DOT,  KC_UP,  KC_ENT,  \
+        KC_LGUI, KC_LALT,          KC_SPACE,LT(_FN,KC_SPACE),LT(_LOWER,KC_ENTER),LSFT_T(KC_SPACE),  KC_LEFT,KC_DOWN,KC_RIGHT  ,KC_RSFT\
     ),
    /* FN */
     /* +-----------------------------------------------------------------------+
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,\
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_MINUS, KC_BSPC, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    LT(_UPPER,KC_V),LT(_LOWER,KC_B),LT(_LOWER,KC_B),LT(_FN,KC_N),KC_M,    KC_COMM, KC_DOT,  KC_UP, KC_ENT,   \
-        KC_LGUI, KC_LALT,          KC_SPACE,LT(_FN,KC_SPACE),LT(_LOWER,KC_ENTER),LSFT_T(KC_SPACE),  KC_LEFT,KC_DOWN,KC_RIGHT KC_RSFT\
+        KC_LGUI, KC_LALT,          KC_SPACE,LT(_FN,KC_SPACE),LT(_LOWER,KC_ENTER),LSFT_T(KC_SPACE),  KC_LEFT,KC_DOWN,KC_RIGHT,KC_RSFT\
     ),
     /* UPPER */
     /* +----------------------------------------------------------------------------------------+
